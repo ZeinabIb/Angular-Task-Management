95884b46c1b7f773a797786dbcde2bf86341a349
zeinab <zeinabibrahim592@gmail.com>
2024-06-02 17:29:01 +0300

    working on tasks

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input, Output, computed, input, signal, EventEmitter } from '@angular/core';
+import { DUMMY_USERS } from '../../dummy-users';
+import { User } from './user.model';
+// type User = {
+//   id: string;
+//   avatar: string;
+//   name: string;
+// };
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  // @Input({required: true}) id!: string;
+  // @Input({required: true}) avatar!: string;
+  // @Input({required: true}) name!: string;
+
+  @Input({required:true}) user!:User;
+  @Input({required:true}) selected!: boolean;
+  @Output() select = new EventEmitter();
+
+
+  get imagePath(){
+    return 'assets/users/' + this.user.avatar;
+  }
+
+  OnSelectUser(){
+    this.select.emit(this.user.id);
+  }
+
+}
